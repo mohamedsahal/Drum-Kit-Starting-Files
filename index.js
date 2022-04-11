c4d3be63a2a47f8bc55226f381246ede3f75a063
mohamedsahal <mohaansahal@gmail.com>
2022-04-11 17:32:00 +0300

    change

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ for (i=0; i<btn; i++){
     
       var text = this.innerHTML;
       switch (text) {
-        case "w": var audio = new Audio("sounds/tom-1.mp3");
-          audio.play();
+        case "w": var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
               
               break;
-        case "a": var audio = new Audio("sounds/tom-2.mp3");
-          audio.play();
+        case "a": var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
                   
          break;
-         case "s": var audio = new Audio("sounds/tom-3.mp3");
-         audio.play();
+         case "s": var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
                  
         break;
-        case "d": var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
+        case "d": var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
                 
        break;
-       case "j": var audio = new Audio("sounds/snare.mp3");
-       audio.play();
+       case "j": var snare = new Audio("sounds/snare.mp3");
+       snare.play();
                
       break;
-      case "k": var audio = new Audio("sounds/crash.mp3");
-      audio.play();
+      case "k": var crash = new Audio("sounds/crash.mp3");
+      crash.play();
               
      break;
-     case "l": var audio = new Audio("sounds/kick-bass.mp3");
-     audio.play();
+     case "l": var kick = new Audio("sounds/kick-bass.mp3");
+     kick.play();
              
     break;
          
